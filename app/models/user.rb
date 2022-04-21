4521f065b75686ced2b0fa3402ff7e22a6b9946b
Mike Mitchell <alchemistikk@gmail.com>
2022-04-21 14:59:10 -0400

    Add Post model with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { in: 4..14 }, uniqueness: true
   validates :password, presence: true, length: { in: 6..20 }
+
+  has_many :posts
 end
